5e08ef1758dda9fdb10c044115ecd85487ad93cb
IgorGustavoZ <igorzerbinatto12@gmail.com>
2026-05-26 14:33:01 -0300

    Adicionar arquivos de projeto.

---
+++ usuario.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http;
+using System.Text.Json;
+
+public class Usuario
+{
+    private readonly HttpClient client = new HttpClient();
+
+    public async Task<List<Usuario>> GetUsuarios()
+    {
+        string url = "http://127.0.0.1:8000/usuarios";
+
+        HttpResponseMessage resposta = await client.GetAsync(url);
+
+        if (!resposta.IsSuccessStatusCode)
+        {
+            return new List<Usuario>();
+        }
+
+        string json = await resposta.Content.ReadAsStringAsync();
+
+        List<Usuario> usuarios = JsonSerializer.Deserialize<List<Usuario>>(json);
+
+        return usuarios;
+    }
+}
